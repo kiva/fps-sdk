6977236c12d731ac4758dd7b101546926706dc87
Matt Raffel <mattr@kiva.org>
2025-06-03 09:02:42 -0600

    corrects auth and staging api endpoint uri information
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- samples/typescript/src/demo/get.loans.ts
+++ samples/typescript/src/demo/get.loans.ts
@@ -14,7 +14,7 @@ const gethAuthToken = async () => {
     log(`getting autho token using client_id '${process.env.client_id}'`);
     log(`\twith secret '${process.env.client_secret}'`);
 
-    const uri = 'https://auth-stage.kiva.org/oauth/token';
+    const uri = 'https://auth.staging.kiva.orgg/oauth/token';
     const details = {
         grant_type: 'client_credentials',
         scope: 'create:loan_draft read:loans',
@@ -53,7 +53,7 @@ const getLoans = async () => {
     // get the auth token since that will be needed for the next call
     await gethAuthToken();
 
-    const uri = `https://partner-api-stage.kiva.org/v3/partner/${partnerId}/loans`;
+    const uri = `https://partnerapi.staging.kiva.org/v3/partner/${partnerId}/loans`;
     log(`calling ${uri}`);
 
     // creating the headers.  authToken value is retrieved from the function
